8331b28b956aa8822adbdd1597026dd68bbfa2e0
Siddhant <20104111.siddhantdarekar@gmail.com>
2026-06-13 21:43:14 +0530

    Transaction Endpoint.

---
+++ src/main/java/com/payflow/payflow_api/controller/TransactionController.java
@@ -0,0 +1,24 @@
+package com.payflow.payflow_api.controller;
+
+
+import com.payflow.payflow_api.entity.Transaction;
+import com.payflow.payflow_api.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public Transaction sendMoney(@RequestBody Transaction transaction){
+
+        return transactionService.sendMoney(transaction);
+    }
+}
